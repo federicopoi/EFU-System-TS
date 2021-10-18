1cc98fdd197469a9ba4d30fe5d8f7649e661e6e9
Federico Poi <fedepoi159@gmail.com>
2021-10-17 22:29:46 -0500

    Project Start 10/17/21

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./App.css";
+import Dashboard from './components/dashboard/Dashboard';
+import Footer from './components/footer/Footer';
+function App() {
+  return (
+    <div className="App">
+      <Dashboard></Dashboard>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
